7937f8c6492eb7d56c73e0577a8a869762d616bc
samuel-udoh <chukwuemekasam16@gmail.com>
2025-07-17 17:13:04 +0100

    task2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """
-Unit tests for the access_nested_map function in the utils module.
-This module contains test cases to verify the correct behavior of
-nested dictionary access functionality.
+Unit tests for the utils.py
 """
 
 import unittest
-from utils import access_nested_map
+from unittest.mock import patch
+import utils
+from utils import access_nested_map, get_json
 from parameterized import parameterized
-from typing import Mapping, Sequence, Any
+from typing import Mapping, Sequence, Any, Dict
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -51,6 +51,33 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """This is the Test case for get_json
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(
+            self, test_url: str, test_payload: Dict, mock_get) -> None:
+        """
+        Test get_json returns json when input is valid.
+
+        Args:
+            test_url: str of url path
+            test_payload: JSON to be return
+        """
+        mock_response = mock_get.return_value
+        mock_response.json.return_value = test_payload
+
+        response = get_json(test_url)
+
+        self.assertEqual(response, test_payload)
+        mock_get.assert_called_once_with(test_url)
 
 
 if __name__ == "__main__":
