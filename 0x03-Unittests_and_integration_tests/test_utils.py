bff8cd4acea3b4696655635067540ab3afa3d21a
samuel-udoh <chukwuemekasam16@gmail.com>
2025-07-17 16:39:17 +0100

    task0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -36,6 +36,22 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ["a"]),
+        ({"a": 1}, ["a", "b"])
+    ])
+    def test_access_nested_map_exception(
+            self, nested_map: Mapping, path: Sequence) -> None:
+        """
+        Test access_nested_map_ returns KeyError for invalid inputs.
+
+        Args:
+            nested_map: A nested dictionary to test
+            path: Sequence of keys to access the nested value
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
 
 if __name__ == "__main__":
     unittest.main()
