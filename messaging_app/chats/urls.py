0deb8c3d56dda28830d0a6d6f136b8027b54bbee
samuel-udoh <chukwuemekasam16@gmail.com>
2025-07-19 15:58:03 +0100

    task 3, 4, 5

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,15 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from .views import ConversationViewSet, MessageViewSet
 from django.urls import path, include
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 
 router.register(r'conversations', ConversationViewSet, basename="conversation")
-router.register(r'messages', MessageViewSet, basename="message")
+
+conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup="conversation")
+conversations_router.register(r'messages', MessageViewSet, basename="conversation-message")
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('', include(conversations_router.urls))
 ]
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+import rest_framework
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls'))
+    path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
