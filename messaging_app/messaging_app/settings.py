925219e360fd36f086120dc60d980532ecb61f23
samuel-udoh <chukwuemekasam16@gmail.com>
2025-07-27 13:18:52 +0100

    task3

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -137,9 +137,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated'
         # 'rest_framework.permissions.AllowAny'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PAGINATION_CLASS': 'messaging_app.chats.pagination.CustomPagination',
     'PAGE_SIZE': 20,
-    
+
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
