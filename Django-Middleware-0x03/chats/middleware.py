d014537b2408ecc5582e0bde9ae78b760798c2d9
samuel-udoh <chukwuemekasam16@gmail.com>
2025-07-27 18:53:11 +0100

    task1 fixing settings order

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,6 +1,7 @@
 import logging
-from datetime import datetime
-
+from datetime import datetime, time
+from rest_framework.status import HTTP_403_FORBIDDEN
+from django.http import JsonResponse
 logger = logging.getLogger(__name__)
 
 class RequestLoggingMiddleware:
@@ -14,3 +15,20 @@ class RequestLoggingMiddleware:
         response = self.get_response(request)
 
         return response
+    
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.block_start = time(21, 0) # 9pm
+        self.block_end = time(6, 0) # 6am
+        
+    def __call__(self, request):
+        current_time = datetime.now().time()
+        
+
+        if (self.block_start <= current_time) or (current_time < self.block_end):
+            return JsonResponse(
+                {'error': 'Requests are not allowed between 9 PM and 6 AM'},
+                status=HTTP_403_FORBIDDEN
+            )
+        return self.get_response(request)
